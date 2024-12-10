532d894e9dedaac18427ad0b5301c6d308a01162
Paulin Kantue <pkantue@gmail.com>
2024-12-10 21:48:22 +0100

    Create Step4_Intro_to_Simulink.m

---
+++ E-Learning/Technical/Matlab/Solar_pump/Step4_Intro_to_Simulink.m
@@ -0,0 +1,8 @@
+% Objective: Provide an overview of Simulink and demonstrate how to create a basic model.
+% Key Topics:
+% Creating a basic Simulink model: drag-and-drop blocks, connecting them, and configuring parameters.
+% Simulating the solar intensity data from MATLAB using a From Workspace block.
+% Visualizing the results with a Scope block.
+% Practical Demonstration:
+% Model a simple system where solar intensity (input) is processed to output electrical power using a gain block for efficiency.
+% include random noise block
