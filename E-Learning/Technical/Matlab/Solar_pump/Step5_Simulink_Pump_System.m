04dc6eec30e26e21c1d141a7fb33113ff00c12d8
Paulin Kantue <pkantue@gmail.com>
2024-12-14 21:06:04 +0100

    Update Step5_Simulink_Pump_System.m

--- E-Learning/Technical/Matlab/Solar_pump/Step5_Simulink_Pump_System.m
+++ E-Learning/Technical/Matlab/Solar_pump/Step5_Simulink_Pump_System.m
@@ -1,9 +1,9 @@
-% Objective: Build a detailed Simulink model to simulate the relationship between solar intensity, panel output, and pump performance.
-% Key Topics:
+% Objective: Build a detailed Simulink Solar panel model to simulate the relationship between solar intensity, panel output.
+% Key Topics: two-part series
 % Adding a MATLAB Function Block to use the noise-added solar intensity data.
+% part1
 % Simulating a solar panel’s behavior using a lookup table or equations.
-% Introducing a Subsystem Block for the pump, using parameters like efficiency and flow rate per kW.
-% Demonstrating conditional logic (e.g., pump shutoff when power is below a threshold) with a Switch Block.
-% Practical Demonstration:
-% Create a model where solar intensity drives the pump’s flow rate, and visualize the system's behavior over time using the Scope and XY Graph blocks.
-% add logic for cyclic loading of data.
+% creating a subsystem of solar panel and connect to current simulation model.
+% part2
+% Add cloud cover to the solar intensity to show the power output of the solar panel.
+% add a bus to show flow of information, including the Save to File.
