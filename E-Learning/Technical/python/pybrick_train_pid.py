dc6dc5d1f22a679c81ccb2705cde0457da6a8f24
Paulin Kantue <pkantue@gmail.com>
2025-01-07 13:34:34 +0100

    added remote object

--- E-Learning/Technical/python/pybrick_train_pid.py
+++ E-Learning/Technical/python/pybrick_train_pid.py
@@ -1,5 +1,5 @@
 from pybricks.hubs import CityHub
-from pybricks.pupdevices import DCMotor, Light, ColorDistanceSensor
+from pybricks.pupdevices import DCMotor, Light, ColorDistanceSensor, Remote
 from pybricks.parameters import Button, Color, Direction, Port, Side, Stop
 from pybricks.robotics import DriveBase
 from pybricks.tools import wait, StopWatch
